db7af1b571112dc3464650db3a15ccfb6b34f52b
Luis Ibanez <luis.ibanez@kitware.com>
2014-01-17 14:06:25 -0500

    Add documentation.

--- INF-202/add_many_students.sh
+++ INF-202/add_many_students.sh
@@ -3,6 +3,8 @@
 # This script adds many users to the system.
 # It must be run with sudo.
 #
-./add_one_user.sh    jdoe     John   Doe     jdoe@albany.edu
-./add_one_user.sh    jsmith   Jane   Smith   jsmith@albany.edu
+#   Script name      username    First    Last    Email
+#
+./add_one_user.sh    jdoe        John     Doe     jdoe@albany.edu
+./add_one_user.sh    jsmith      Jane     Smith   jsmith@albany.edu
 #... add more names here..
