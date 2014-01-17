ca8ef350ae02bf8275bdf2638a94c4c14e21c8e0
Luis Ibanez <luis.ibanez@kitware.com>
2014-01-17 14:04:51 -0500

    Create many student accounts.

---
+++ INF-202/add_many_students.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# This script adds many users to the system.
+# It must be run with sudo.
+#
+./add_one_user.sh    jdoe     John   Doe     jdoe@albany.edu
+./add_one_user.sh    jsmith   Jane   Smith   jsmith@albany.edu
+#... add more names here..
