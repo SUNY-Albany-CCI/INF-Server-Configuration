ad92c2a0d6fa50baa9e7c54e839f43f7112b2558
Luis Ibanez <luis.ibanez@kitware.com>
2014-01-20 19:54:58 -0500

    Installing more packages.
    Emacs editor, and setup to be able to send email.

--- INF-202/initial_setup.sh
+++ INF-202/initial_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-
+sudo apt-get install apt-file
+sudo apt-file update
+sudo apt-get install emacs
+sudo apt-get install sendemail
+sudo apt-get install libio-socket-ssl-perl  libnet-ssleay-perl
