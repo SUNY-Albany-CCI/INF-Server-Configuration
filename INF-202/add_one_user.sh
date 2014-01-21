47992328026db6618b2c344c53df29de48e58127
Luis Ibanez <luis.ibanez@kitware.com>
2014-01-20 20:34:21 -0500

    Added extra parameters to send email

--- INF-202/add_one_user.sh
+++ INF-202/add_one_user.sh
@@ -17,13 +17,15 @@ privatekey="$keyname"
 publickey="$keyname.pub"
 email=$4
 passphrase=$5
+professoremailusername=$6
+professoremailpassword=$7
 
 adduser \
 --gecos "$fullname,,," \
  $username
 
 # Create SSH keys
-./generate_one_ssh_key.sh $keyname $email $passphrase
+./generate_one_ssh_key.sh $keyname $email $passphrase $professoremailusername $professoremailpassword
 
 # Install SSH key
 sshdir="/home/$username/.ssh"
