d82bfa73d9f3c2e9da6528a5c4dcd5661ccc8434
Luis Ibanez <luis.ibanez@kitware.com>
2014-01-21 20:24:31 -0500

    Added script to clone git repostories.
    And to expose them in the Apache server directory  /var/www.

---
+++ INF-202/deploy_learning_encounters.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Run this script as sudo
+#
+cd /var/www
+mkdir -p LearningEncounters
+cd LearningEncounters
+
+SUNYGithub=https://github.com/SUNY-Albany-CCI-LearningEncounters
+
+git clone --recursive $SUNYGithub/EWD-JS-Introduction-Part-I.git
+git clone --recursive $SUNYGithub/RDF-Introduction-Part-I.git
+git clone --recursive $SUNYGithub/ITLAL-Seminar-Activities.git
+git clone --recursive $SUNYGithub/Encounter-Template-Reveal.js.git
+git clone --recursive $SUNYGithub/Creating-Learning-Encounters.git
+git clone --recursive $SUNYGithub/Git-Part-I.git
+git clone --recursive $SUNYGithub/Linux-Kernel-Training-Session-I.git
+git clone --recursive $SUNYGithub/Evolution-Of-Cooperation.git
+git clone --recursive $SUNYGithub/Raspberry-Pi-Web-Server-With-Python.git
+git clone --recursive $SUNYGithub/Who-Killed-Aaron-Swartz.git
+
+
